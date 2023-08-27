16bce567d3ab1d4165f792606d29aa4ca51d45dc
Tanisha Bansal <100759293+tanisha12j@users.noreply.github.com>
2023-08-27 23:08:29 +0530

    Added solution - LeetHub

---
+++ Missing number in array - GFG/missing-number-in-array.cpp
@@ -0,0 +1,49 @@
+//{ Driver Code Starts
+// Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+
+// } Driver Code Ends
+// User function template for C++
+
+class Solution{
+  public:
+    int missingNumber(vector<int>& array, int n) {
+        // Your code goes here
+        int i=1;
+        int sum=0;
+        while(i<=n)
+        {
+            sum+=i;
+            i++;
+        }
+        int s=0;
+        for(auto i : array)
+        {
+            s+=i;
+        }
+        return sum-s;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+
+        vector<int> array(n - 1);
+        for (int i = 0; i < n - 1; ++i) cin >> array[i];
+        Solution obj;
+        cout << obj.missingNumber(array, n) << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
